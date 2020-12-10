8c6e678ce708a04ca0319019df5e0333cc1741b2
im_wower <george674834080@gmail.com>
2020-12-10 11:06:37 +0800

    get current user

--- src/main/java/com/example/demo/controller/DemoController.java
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -1,16 +1,26 @@
 package com.example.demo.controller;
 
+import com.example.demo.model.User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.Arrays;
 import java.util.List;
 
 @RestController
 public class DemoController {
 
-    @GetMapping("/demo")
+    @GetMapping("/demo/list")
     public List<String> getList() {
         return Arrays.asList("abc", "def");
+    }
+
+    @GetMapping("/demo/currentUser")
+    public User getCurrentUser(HttpServletRequest request) {
+        User user = new User();
+        String name = request.getHeader("user_name");
+        user.setName(name);
+        return user;
     }
 }
